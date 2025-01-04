eb3333e6c1ab0cea044c4562b488b83e06d0e6fc
Hooria Arshad <hooriaarshad37@gmail.com>
2025-01-05 01:53:02 +0500

    commit message

---
+++ app.py
@@ -0,0 +1,202 @@
+# print("Hello world")
+# name:str ="hoor"
+# age:int ="20"
+# is_active:bool=True
+
+# # yaha ma variable ko override kr rhi ho
+
+# name="hoora"
+# print(name)
+
+# num1: int= 5
+# num2: int= 10
+
+# print(num1*num2)
+
+# allowed:bool=False
+
+# print(allowed)
+# print(8*6)
+# print(9/5)
+# print(10+10)
+# print(10-5)
+
+# # a=19  (Numeric)
+# # b="hoor" (Text Data)
+# # c=True  (Boolean True/False)
+# # d=None
+
+# #print(a)
+# #print(b)
+# #print(c)
+# #print(d)
+
+# a=100
+# b=200
+# print(int(a)+int(b))
+
+# num1=12
+# num2=12
+# print(num1 + num2)
+
+# a="hoora"
+# b="arshad"
+
+# print(a+b)
+
+# massage:str="hoor"
+# print(massage)
+
+# #  Types of operators 
+
+# #  1. Arthematic operators.
+# #  2. Assignment operators.
+# #  3. Comparison operators. (boolean) True / False
+# #  4. Logical operators.
+
+
+
+# # 1. Arthematical operators
+
+# # print(7+5)
+# # print(10-5)
+# # print(10/2)
+# # print(5*2) 5 * 2
+# # print(8 ** 2) exponent 8^2= 8 x 8 = 64 
+# # print(11 // 2) floor division 
+# # print (11 % 2) 11 / 2 = 1  remainder / modulus
+
+# num1: int = 5
+# num2: int = 5
+
+# #            5  +  5
+# # sum: int = num1 + num2
+# # sub: int = num1 - num2 
+# # div: int = num1 / num2 
+# # mul: int = num1 * num2 
+# # print(mul) 
+
+
+# # 2. Assignment operators
+# #  (=)
+# # x(varaible) 
+
+# # 15 + 5
+# # x = x + 5
+# # addition
+# # x = x - 5
+# # subtaction
+
+# # x = x * 2
+# # multiplication
+
+# x: int = 15
+# # 15 * 2 = 30
+# x = x % 3 
+# # remainder / modulus
+# # answer = 0
+
+# print(x)
+
+# # 3. Comparision operators  True / False
+# # ==  (equal)
+# # >   (greater than)
+# # <   (less than)
+# # >=  (greater or equal to)
+# # <=  (less or equal to)
+# # !=  (not equal to)
+
+# # meri first  operand / value second waly operand /value ke baraber han
+
+# num1: int = 10
+# num2: int = 20 
+
+# # print(10 == 20)
+
+# print (num1 == num2) 
+# print(num2 > num1)
+# print(num2 < num1)
+
+# num1: int = 10
+# num2: int = 11
+
+# # print(10 == 10)
+
+# print(num1 >= num2)
+# print(num1 <= num2)
+# print(num1 != num2)
+
+# # 4. Logical operators 
+
+# # and 
+# # or 
+# # not
+
+# num1: int = 40
+# # num2: int = 11
+# #  condtion
+# # num1 is greater than 10
+# # AND
+# # num1 is less than 30
+# num1=40
+# num1 =8
+# num1 = False
+# #  True   and False
+# print(num1 > 10 )
+# print(num1 < 20 )
+# print(num1 < 30 )
+#  #  True  andTrue  andFalse
+# print(num1 > 10 and num1 > 20 and num1 < 30)
+
+
+# # #  or
+
+# num1: int = 40
+
+# print(num1 > 10 or num1 < 40 ) #dono conditions true honi chahiye
+#  #           True     ya    False
+# print(num1 > 10 or num1 < 40)
+
+# print(not 5 == 4)
+
+
+
+#  #  ASSIGNMENT
+
+# # k-electric = " no k-electric"
+# # ups= "ups"
+
+# name1: str =  "no k-electric"
+# name2: str =  "ups"
+
+# print(name1 ==  name2 )
+
+#  If    Else
+# # AGAR WARNA
+# num1: int =2
+# num2: int =1
+# if num1 > num2:
+#      print("Hello")
+#      uname="hoor"
+#      print(uname)
+
+
+# a=True
+# b=False
+# print(a or b) 
+# a = 2
+# b = 5
+# sum = a + b
+# print(sum)
+
+
+# name1: str = "HOOR"
+# name2: str = "ARSHAD"
+# if name1 == name2:
+#   print(False)   
+# else:
+#      print(True)
+
+
+Name =input("enter your name")
+print("congrats your name is", Name)
